fd05809b82c8962d20aec78bcd9c5d145eba3d8a
MegaDrage <dragonpsui@gmail.com>
2024-08-18 22:41:48 +0700

    xplr: rm plugins

--- home/xplr.nix
+++ home/xplr.nix
@@ -1,19 +1,15 @@
-{ fetchFromGitHub }: {
+{ fetchFromGitHub, ... }: {
   programs.xplr = {
     enable = true;
-    plugins = [{
-      wl-clipboard = fetchFromGitHub {
-        owner = "sayanarijit";
-        repo = "wl-clipboard.xplr";
-        rev = "a3ffc87460c5c7f560bffea689487ae14b36d9c3";
-        hash = "sha256-I4rh5Zks9hiXozBiPDuRdHwW5I7ppzEpQNtirY0Lcks=";
-      };
-      nuke = {
-        owner = "Junker";
-        repo = "nuke.xplr";
-        rev = "a3ffc87460c5c7f560bffea689487ae14b36d9c3";
-        hash = "sha256-I4rh5Zks9hiXozBiPDuRdHwW5I7ppzEpQNtirY0Lcks=";
-      };
-    }];
+    # plugins = [
+      # {
+        # wl-clipboard = fetchFromGitHub {
+        # owner = "sayanarijit";
+        # repo = "wl-clipboard.xplr";
+        # rev = "a3ffc87460c5c7f560bffea689487ae14b36d9c3";
+        # hash = "sha256-I4rh5Zks9hiXozBiPDuRdHwW5I7ppzEpQNtirY0Lcks=";
+        # };
+      # }
+    # ];
   };
 }
