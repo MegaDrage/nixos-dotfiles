5e500dd70841c9a47df0c70551c3acdca3926435
MegaDrage <dragonpsui@gmail.com>
2024-08-22 10:42:29 +0700

    Fmt n rm eza alliases

--- home/zsh.nix
+++ home/zsh.nix
@@ -1,4 +1,4 @@
-{config, ...}:{
+{ config, ... }: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -9,18 +9,21 @@
       path = "${config.xdg.dataHome}/zsh/history";
     };
     shellAliases = {
-      ls="eza --color=always --group-directories-first --icons";
-      ll="eza -la --icons --octal-permissions --group-directories-first";
-      l="eza -bGF --header --git --color=always --group-directories-first --icons";
-      llm="eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons"; 
-      la="eza --long --all --group --group-directories-first";
-      lx="eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons";
+      # ls = "eza --color=always --group-directories-first --icons";
+      # ll = "eza -la --icons --octal-permissions --group-directories-first";
+      # l =
+      #   "eza -bgf --header --git --color=always --group-directories-first --icons";
+      # llm =
+      #   "eza -lbgd --header --git --sort=modified --color=always --group-directories-first --icons";
+      # la = "eza --long --all --group --group-directories-first";
+      # lx =
+      #   "eza -lbhhigumusa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons";
 
-      lS="eza -1 --color=always --group-directories-first --icons";
-      lt="eza --tree --level=2 --color=always --group-directories-first --icons";
-      ldot="eza -a | grep -E '^\.'";
-      vim="nvim";
+      # lS = "eza -1 --color=always --group-directories-first --icons";
+      # lt =
+      #   "eza --tree --level=2 --color=always --group-directories-first --icons";
+      # ldot = "eza -a | grep -E '^.'";
     };
-    initExtra = "eval \"$(direnv hook zsh)\"";
+    initExtra = ''eval "$(direnv hook zsh)"'';
   };
 }
