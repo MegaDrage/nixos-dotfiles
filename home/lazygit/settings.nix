022e6b3c91db1b108c061924facfa58f5cac088c
MegaDrage <dragonpsui@gmail.com>
2024-08-22 11:54:14 +0700

    Edit settings for lg

--- home/lazygit/settings.nix
+++ home/lazygit/settings.nix
@@ -1,7 +1,28 @@
 {
-  # If true, show a confirmation popup before quitting Lazygit
-  confirmOnQuit = true;
+  showListFooter = true;
 
+  # If true, display the files in the file views as a tree. If false, display the files as a flat list.
+  # This can be toggled from within Lazygit with the '~' key, but that will not change the default.
+  showFileTree = true;
+
+  # If true, show a random tip in the command log when Lazygit starts
+  showRandomTip = true;
+
+  # If true, show the command log
+  showCommandLog = true;
+
+  # If true, show the bottom line that contains keybinding info and useful buttons. If false, this line will be hidden except to display a loader for an in-progress action.
+  showBottomLine = true;
+  # Nerd fonts version to use.
+  # One of: '2' | '3' | empty string (default)
+  # If empty, do not show icons.
+  nerdFontsVersion = "3";
+
+  # If true (default), file icons are shown in the file views. Only relevant if NerdFontsVersion is not empty.
+  showFileIcons = true;
+
+  # If true, show jump-to-window keybindings in window titles.
+  showPanelJumps = true;
   # If true, exit Lazygit when the user presses escape in a context where there is nothing to cancel/close
   quitOnTopLevelReturn = true;
 }
