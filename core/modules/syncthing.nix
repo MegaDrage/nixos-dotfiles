4511248102a925d9098b1102f3b96041c0798722
MegaDrage <dragonpsui@gmail.com>
2024-08-23 16:11:17 +0700

    core/modules/ syncthing.nix: add sync config

--- core/modules/syncthing.nix
+++ core/modules/syncthing.nix
@@ -1,3 +1,35 @@
-{
-
+{ config, ... }: {
+  services = {
+    syncthing = {
+      enable = true;
+      user = "megadrage";
+      dataDir = "$HOME/Documents";
+      configDir = "${config.xdg.configHome}/syncthing";
+      overrideDevices = true;
+      overrideFolders = true;
+      guiAddress = "0.0.0.0:8384";
+      settings = {
+        devices = {
+          "device1" = {
+            id =
+              "KOX2HLQ-A5HCQGN-TT6M2JS-HRHLFHJ-DWO57II-YZ7HHAS-KBKFYGV-EPEQGQ4";
+          };
+        };
+        folders = {
+          "GTD" = {
+            path = "$HOME/Documents/GTD/";
+            devices = [ "device1" "device2" ];
+            versioning = {
+              type = "simple";
+              params = { keep = "3"; };
+            };
+          };
+        };
+        gui = {
+          user = "megadrage";
+          password = "1234";
+        };
+      };
+    };
+  };
 }
