ed842e59bf4f2fecd1bc53f26a0e42d8cc3d0285
MegaDrage <dragonpsui@gmail.com>
2024-08-23 14:33:27 +0700

    home/zsh.nix: add autocd n antidote plug mngr

--- home/zsh.nix
+++ home/zsh.nix
@@ -4,6 +4,8 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    autocd = true;
+    antidote = { enable = true; plugins = []; };
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
