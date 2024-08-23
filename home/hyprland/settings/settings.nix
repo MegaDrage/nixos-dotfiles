900f83fad74d4c877a780a40e187d62eb7ec825b
MegaDrage <dragonpsui@gmail.com>
2024-08-23 21:33:44 +0700

    core/modules/ hypr.nix: add hyprland system-wide, home/hyprland/ add hyrpland hm settings/ add settings

---
+++ home/hyprland/settings/settings.nix
@@ -0,0 +1,86 @@
+{
+  monitor = [ ",preferred,auto,1" ];
+  windowrule = [ "float, pavucontrol" ];
+  env = [
+    "NIXOS_OZONE_WL, 1"
+    "NIXPKGS_ALLOW_UNFREE, 1"
+    "XDG_CURRENT_DESKTOP, Hyprland"
+    "XDG_SESSION_TYPE, wayland"
+    "XDG_SESSION_DESKTOP, Hyprland"
+    "GDK_BACKEND, wayland, x11"
+    "CLUTTER_BACKEND, wayland"
+    "QT_QPA_PLATFORM=wayland;xcb"
+    "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
+    "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
+    "SDL_VIDEODRIVER, x11"
+    "MOZ_ENABLE_WAYLAND, 1"
+  ];
+  general = {
+    gaps_in = 7;
+    gaps_out = 7;
+    border_size = 1;
+    allow_tearing = true;
+    resize_on_border = true;
+  };
+  decoration = {
+    rounding = 16;
+    blur = {
+      enabled = true;
+      brightness = 1.0;
+      contrast = 1.0;
+      noise = 1.0e-2;
+
+      vibrancy = 0.2;
+      vibrancy_darkness = 0.5;
+
+      passes = 4;
+      size = 7;
+
+      popups = true;
+      popups_ignorealpha = 0.2;
+    };
+
+    drop_shadow = true;
+    shadow_ignore_window = true;
+    shadow_offset = "0 15";
+    shadow_range = 100;
+    shadow_render_power = 2;
+    shadow_scale = 0.97;
+    "col.shadow" = "rgba(00000055)";
+  };
+  animations = {
+    enabled = true;
+    animation = [
+      "border, 1, 2, default"
+      "fade, 1, 4, default"
+      "windows, 1, 3, default, popin 80%"
+      "workspaces, 1, 2, default, slide"
+    ];
+  };
+
+  input = {
+    kb_layout = "us,ru";
+    touchpad = { natural_scroll = true; };
+    follow_mouse = 1;
+    kb_options = "grp:alt_shift_toggle";
+  };
+  dwindle = {
+    pseudotile = true;
+    preserve_split = true;
+  };
+
+  misc = {
+    disable_autoreload = true;
+    always_follow_on_dnd = true;
+    layers_hog_keyboard_focus = true;
+    animate_manual_resizes = false;
+    enable_swallow = true;
+    focus_on_activate = true;
+    force_default_wallpaper = 0;
+    animate_mouse_windowdragging = false;
+    vrr = 1;
+  };
+
+  xwayland.force_zero_scaling = true;
+  debug.disable_logs = false;
+}
