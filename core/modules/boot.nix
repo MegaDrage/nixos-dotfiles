ef2b60d9277ac54ebe6b2611f32025634112bd92
MegaDrage <dragonpsui@gmail.com>
2025-01-05 10:57:32 +0700

    core/modules/boot.nix: change boot for my sys

--- core/modules/boot.nix
+++ core/modules/boot.nix
@@ -1,20 +1,14 @@
 { pkgs, ... }: {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [
-      "kvm.enable_virt_at_load=0"
-      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-      "systemd.mask=systemd-vconsole-setup.service"
-      "systemd.mask=dev-tpmrm0.device" # this is to mask that stupid 1.5 mins systemd bug
-      "nowatchdog"
-      "modprobe.blacklist=sp5100_tco" # watchdog for AMD
-    ];
+    kernelModules = [ "amdgpu "];
+    # kernelParams = [ ];
 
-    initrd = {
-      availableKernelModules =
-        [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-      kernelModules = [ ];
-    };
+    # initrd = {
+    #   availableKernelModules =
+    #     [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+    #   kernelModules = [ ];
+    # };
 
     supportedFilesystems = [ "ntfs" ];
     loader = {
