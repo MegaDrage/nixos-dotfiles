df00197d0d4b08884e2c2acc7819c13a8ca66b6d
MegaDrage <dragonpsui@gmail.com>
2025-01-12 14:02:35 +0700

    feat: font_size and themeFile for kitty
    14 font size
    Tokyo Night Moon theme

--- home/kitty/default.nix
+++ home/kitty/default.nix
@@ -2,8 +2,9 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
-    # themeFile = "Catppuccin-Macchiato";
+    themeFile = "tokyo_night_moon";
     settings = {
+      font_size = "14.0";
       disable_ligatures = "cursos";
       shell_integration = "enabled";
       scrollback_lines = "2000";
