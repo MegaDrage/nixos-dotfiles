e1a5d157712d4c385306c8d24097115c0a295bd4
MegaDrage <dragonpsui@gmail.com>
2025-01-12 14:01:54 +0700

    feat: dconf base settings, also hypridle with gnome settings

--- home/dconf.nix
+++ home/dconf.nix
@@ -1,5 +1,27 @@
-{
-  dconf = {settings = {
-"/org/gnome/desktop/interface/" = { monospace-font-name = "JetBrains Mono NL Medium 10"; text-scaling-factor = "1.15";};
-      };};
+{ pkgs, ... }: {
+  dconf = {
+    settings = {
+      "org/gnome/desktop/interface" = {
+        monospace-font-name = "JetBrainsMonoNL Nerd Font Mono 14";
+        font-name = "Inter Medium 11";
+        document-font-name = "Inter 11";
+        text-scaling-factor = 1.1;
+        font-hinting = "none";
+        font-antialising = "rgba";
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions =
+          [ "user-theme@gnome-shell-extensions.gcampax.github.com" ];
+      };
+      "org/gnome/shell/extensions/user-theme" = { name = "Nightfox-Dark"; };
+      "org/gnome/settings-daemon/plugins/power" = {
+        idle-dim = false;
+        idle-delay = 0;
+        sleep-inactive-ac-type = "nothing";
+        sleep-inactive-battery-type = "nothing";
+      };
+    };
+  };
+  home.packages = with pkgs; [ gnomeExtensions.user-themes gnome-themes-extra gtk-engine-murrine ];
 }
