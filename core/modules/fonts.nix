d66c355b9c5ab0116e0bd6ac0c00a0bc916270bb
MegaDrage <dragonpsui@gmail.com>
2025-01-12 14:00:32 +0700

    feat: new system fonts

--- core/modules/fonts.nix
+++ core/modules/fonts.nix
@@ -2,7 +2,9 @@
   fonts = {
     packages = with pkgs; [
       # nerd-fonts.jetbrains-mono
-      jetbrains-mono
+      nerd-fonts.jetbrains-mono
+      ibm-plex
+      inter
       nerd-fonts.iosevka
       nerd-fonts.fira-code
       corefonts
