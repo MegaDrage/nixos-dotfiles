6dee267da2b25070e93a8e5ab5b73c717fdb0f87
MegaDrage <dragonpsui@gmail.com>
2025-01-12 14:00:21 +0700

    feat: plymouth + kernel params for quiet loading

--- core/modules/boot.nix
+++ core/modules/boot.nix
@@ -1,15 +1,26 @@
 { pkgs, ... }: {
   boot = {
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelModules = [ "amdgpu "];
-    # kernelParams = [ ];
-
-    # initrd = {
-    #   availableKernelModules =
-    #     [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-    #   kernelModules = [ ];
-    # };
-
+    kernelModules = [ "amdgpu " ];
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+    plymouth = {
+      enable = true;
+      theme = "breeze";
+      # themePackages = with pkgs;
+      #   [
+      #     (adi1090x-plymouth-themes.override { selected_themes = [ "rings" ]; })
+      #   ];
+    };
+    consoleLogLevel = 0;
+    initrd.verbose = false;
     supportedFilesystems = [ "ntfs" ];
     loader = {
       efi = {
