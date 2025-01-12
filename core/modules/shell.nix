35ac47a53a58cc88e36351043280edd88fce928e
MegaDrage <dragonpsui@gmail.com>
2025-01-12 13:58:23 +0700

    ref: remove aliases from systemwide to user

--- core/modules/shell.nix
+++ core/modules/shell.nix
@@ -1,16 +1,3 @@
 { pkgs, ... }: {
   users.defaultUserShell = pkgs.zsh;
-  environment = {
-    shellAliases = {
-      l =
-        "eza -bgf --header --git --color=always --group-directories-first --icons always";
-      llm =
-        "eza -lbgd --header --git --sort=modified --color=always --group-directories-first --icons always";
-      lS = "eza -1 --color=always --group-directories-first --icons always";
-      lt =
-        "eza --tree --level=3 --color=always --group-directories-first --icons always";
-      ldot = "eza -a | grep -E '^.'";
-      vim = "nvim";
-    };
-  };
 }
