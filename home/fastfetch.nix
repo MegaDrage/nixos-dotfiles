dc7c4cce65cbb1dfef134a0a988f0934a24a473b
MegaDrage <dragonpsui@gmail.com>
2025-01-12 14:01:21 +0700

    ref: ff mv into home

--- home/fastfetch.nix
+++ home/fastfetch.nix
@@ -1,1 +1,39 @@
-{ programs.fastfetch = { enable = true; }; }
+{
+  programs.fastfetch = {
+    enable = true;
+    # settings = {
+    #   logo = {
+    #     source = "nixos_small";
+    #     padding = { right = 1; };
+    #   };
+    #   display = {
+    #     size = { binaryPrefix = "si"; };
+    #     color = "blue";
+    #     separator = "  ";
+    #   };
+    #   modules = [
+    #     {
+    #       type = "datetime";
+    #       key = "Date";
+    #       format = "{1}-{3}-{11}";
+    #     }
+    #     {
+    #       type = "datetime";
+    #       key = "Time";
+    #       format = "{14}:{17}:{20}";
+    #     }
+    #     "break"
+    #     "player"
+    #     "media"
+    #     "break"
+    #     {
+    #       type = "disk";
+    #       mountpoints =
+    #         [ "/" "/home" ];
+    #       format =
+    #         "{mountpoint}: {used}/{total} ({percentage_used})";
+    #     }
+    #   ];
+    # };
+  };
+}
