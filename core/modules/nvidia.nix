a55f831a4d7d8f8721f0eca7c96285e3704c8895
MegaDrage <dragonpsui@gmail.com>
2025-02-05 15:20:21 +0700

    ref: new nvidia config - working....

--- core/modules/nvidia.nix
+++ core/modules/nvidia.nix
@@ -1,22 +1,8 @@
 { lib, config, ... }: {
   services.xserver.videoDrivers = [ "nvidia" ];
-  environment = {
-    variables = {
-      LIBVA_DRIVER_NAME = "nvidia";
-      XDG_SESSION_TYPE = "wayland";
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      __GL_GSYNC_ALLOWED = "1";
-      __GL_VRR_ALLOWED = "0";
-      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      CUDA_CACHE_PATH = "$XDG_CACHE_HOME/nv";
-    };
-  };
-
   hardware = {
 
-    # amdgpu.initrd.enable = false;
+    amdgpu.initrd.enable = false;
 
     nvidia = {
       nvidiaPersistenced = lib.mkDefault true;
@@ -24,11 +10,12 @@
 
       nvidiaSettings = lib.mkDefault true;
 
-      powerManagement.enable = lib.mkDefault true;
+      powerManagement = {enable = lib.mkDefault true; finegrained = true;};
 
       open = false;
 
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+      dynamicBoost.enable = true;
 
       prime = {
         offload = {
